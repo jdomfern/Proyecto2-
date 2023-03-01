1242afaff909f4510f2bd6344b2c67d8e32a4cfd
Jorge <Jorge@DESKTOP-5FAD0MS>
2023-03-01 21:41:01 +0100

    Introduccion de Try catch en escaner

--- Proyecto2Tri/src/Metodos.java
+++ Proyecto2Tri/src/Metodos.java
@@ -15,26 +15,40 @@ public class Metodos {
 		
 		String nombre,sexo,ciudad,fisico;
 		int edad;
-		boolean bueno=false;
+		boolean correcto=true;
 		Scanner sc=new Scanner (System.in);
-		System.out.println("Añade un@ nuev@ usuari@s");
-		System.out.println("Introduce nombre");
-		nombre=sc.nextLine();
-		System.out.println("Introduce edad");
-		edad=sc.nextInt();
-		System.out.println("Introduce sexo");
-		sexo=sc.nextLine();
-		System.out.println("Introduce ciudad");
-		ciudad=sc.nextLine();
-		System.out.println("Está buen@?");
-		fisico=sc.nextLine();
-		if (fisico.equals("SI")){
-		bueno=true;}
+		while(correcto) {
+		try {
+			
+			System.out.println("Añade un@ nuev@ usuari@s");
+			System.out.println("Introduce nombre");
+			nombre=sc.nextLine();
+			System.out.println("Introduce sexo");
+			sexo=sc.nextLine();
+			System.out.println("Introduce ciudad");
+			ciudad=sc.nextLine();
+			System.out.println("Está buen@?");
+			fisico=sc.nextLine();
+			System.out.println("Introduce edad");
+			edad=sc.nextInt();
+			
+			correcto=false;
+			
+			DatosPersonas persona= new DatosPersonas(nombre, edad, sexo, ciudad, fisico);
+			DatosPersonas [] arrayPersona = new DatosPersonas[10];
+			for (int i=0;i<arrayPersona.length;i++) {
+			arrayPersona[i]=persona;
+		}
+		}
+		catch (Exception e) {
+			System.out.println("Error al introducir el dato");
+			sc.next();
+		}
 		
-		DatosPersonas persona= new DatosPersonas(nombre, edad, sexo, ciudad, fisico);
-		DatosPersonas [] arrayPersona = new DatosPersonas[10];
-		for (int i=0;i<arrayPersona.length;i++) {
-		arrayPersona[i]=persona;
+		}
+	
+		
+
 	}
 	}
-}
+
